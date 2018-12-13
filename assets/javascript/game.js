0056251929c7eab7e20e54a5ac9a6a019ae628d1
hliang <hliang@github>
2018-12-13 09:37:06 -0600

    add game.js; slight change to css

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var charList = [
+        {name: "Mario", icon: "assets/images/mario.png", hp: 120, attackPower: 20, counterPower: 5},
+        {name: "Luigi", icon: "assets/images/luigi.png", hp: 120, attackPower: 20, counterPower: 5},
+        {name: "Toad", icon: "assets/images/toad.png", hp: 120, attackPower: 20, counterPower: 5},
+        {name: "Yoshi", icon: "assets/images/yoshi.png", hp: 120, attackPower: 20, counterPower: 5},
+        {name: "Bowser", icon: "assets/images/bowser.png", hp: 120, attackPower: 20, counterPower: 5},
+    ];
+
+
+
+});
